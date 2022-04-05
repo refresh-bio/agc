967679066d989a2541f1f56254291b7cfc607d77
Sebastian Deorowicz <sebastian.deorowicz@polsl.pl>
2022-04-05 15:53:16 +0200

    Version info update

--- src/core/defs.h
+++ src/core/defs.h
@@ -20,7 +20,7 @@ typedef vector<uint8_t> packed_block_t;
 
 const uint32_t AGC_VER_MAJOR = 2;
 const uint32_t AGC_VER_MINOR = 0;
-const string AGC_VER_BUILD = "20220317.1"s;
+const string AGC_VER_BUILD = "20220405.1"s;
 const uint32_t AGC_FILE_MAJOR = 2;
 const uint32_t AGC_FILE_MINOR = 0;
 
